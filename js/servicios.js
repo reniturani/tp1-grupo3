384fa50572aa7b91a7bd208df18051a28b995191
AriLaviz <lavizzariariadna@gmail.com>
2026-05-12 18:23:08 -0300

    Hice servicios.js, y tuve que modificar el css debido a que estaban las tarejetas por separado y al haber 13 servicios que agregar seria muy extenso, asi que lo modifique para que sea uno general, tambien agregue flexbox

---
+++ js/servicios.js
@@ -0,0 +1,29 @@
+const cardContainer = document.querySelector('#card-container')
+
+async function servicios () {
+    try {
+        const response = await fetch('http://localhost:3000/servicios')
+        const data = await response.json()
+        console.log(data)
+
+        data.forEach(servicio => {
+            const div = document.createElement('div')
+            div.classList.add('tarjeta-servicio')
+
+            div.innerHTML = `
+                <h2>💾Instalación de software, hardware y SO</h2>
+                <img src = "../assets/sistem_op.jpg" width="300" height="200">
+                <p>Precio: <span class="precio">${servicio.precio}</span></p>
+                <h3>${servicio.desc}</h3>
+            `
+
+            cardContainer.append(div)
+        })
+    }catch {
+        console.log('Error, no se pudieron traer los datos de los servicios.')
+    }
+
+
+}
+
+servicios()
